d709cff8285e3b90f8a4f96f410e79dd8b42be1b
Majedul Hoque <rumanbsl@gmail.com>
2018-08-23 21:40:35 +0300

    meh

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-/**
- * 1. get arguments from cli and check if it is available in a text file
- */
-
 extern crate minigrep;
 use minigrep::Config;
 use std::process;
 
-fn main(){
+fn main() {
   let config = Config::new(std::env::args()).unwrap_or_else(|err_message| {
     eprintln!("{}", err_message);
     process::exit(1);
